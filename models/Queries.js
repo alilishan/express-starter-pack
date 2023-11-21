7586921a95684e1a187c31b211ada66272b2b428
Ali Lishan <alilishan@gmail.com>
2023-11-21 13:58:02 +0800

    Update Queries.js

--- models/Queries.js
+++ models/Queries.js
@@ -36,3 +36,42 @@ exports.queryPagination = [
         .notEmpty().withMessage('Status cannot be empty')
         .isIn(Object.keys(statuses)).withMessage(`Status value can be either ${Object.keys(statuses).join(', ')}`),
 ]
+
+exports.checkPagination = [
+    check('skip')
+        .if(check('take').exists())
+        .exists().withMessage('Skip is needed for pagination')
+        .isNumeric().withMessage('Skip must be a number')
+        .notEmpty().withMessage('Skip cannot be empty'),
+
+    check('take')
+        .if(check('skip').exists())
+        .exists().withMessage('Take is needed for pagination')
+        .isNumeric().withMessage('Take must be a number')
+        .notEmpty().withMessage('Take cannot be empty'),
+
+    check('startTimestamp')
+        .if(check('endTimestamp').exists())
+        .exists().withMessage('Start timestamp is needed for pagination')
+        .isNumeric().withMessage('Start timestamp must be a number')
+        .notEmpty().withMessage('Start timestamp cannot be empty'),
+
+    check('endTimestamp')
+        .if(check('startTimestamp').exists())
+        .exists().withMessage('End timestamp is needed for pagination')
+        .isNumeric().withMessage('End timestamp must be a number')
+        .notEmpty().withMessage('End timestamp cannot be empty'),
+
+    check('orderBy')
+        .optional()
+        .if(check('orderSort').exists())
+        .exists().withMessage('Order by is needed for pagination')
+        .notEmpty().withMessage('Order by cannot be empty'),
+
+    check('orderSort')
+        .optional()
+        .if(check('orderBy').exists())
+        .exists().withMessage('Order sort is needed for pagination')
+        .notEmpty().withMessage('Order sort cannot be empty')
+        .isIn(['desc', 'asc']).withMessage(`Order sort value can be either ${['desc', 'asc'].join(', ')}`),
+]
